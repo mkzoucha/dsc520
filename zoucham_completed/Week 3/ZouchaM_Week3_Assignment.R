5713d3479481868889193354733a6d7225c83734
mkzoucha <mzoucha@my365.bellevue.edu>
2021-09-13 18:51:16 -0600

    Changed scatterplots to filled circles

--- zoucham_completed/Week 3/ZouchaM_Week3_Assignment.R
+++ zoucham_completed/Week 3/ZouchaM_Week3_Assignment.R
@@ -15,29 +15,29 @@ heights_df <- read.csv("data/r4ds/heights.csv")
 # https://ggplot2.tidyverse.org/reference/geom_point.html
 ## Using `geom_point()` create three scatterplots for
 ## `height` vs. `earn`
-ggplot(heights_df, aes(x=height, y=earn)) + geom_point(shape=1)
+ggplot(heights_df, aes(x=height, y=earn)) + geom_point()
 ## `age` vs. `earn`
-ggplot(heights_df, aes(x=age, y=earn)) + geom_point(shape=1)
+ggplot(heights_df, aes(x=age, y=earn)) + geom_point()
 ## `ed` vs. `earn`
-ggplot(heights_df, aes(x=ed, y=earn)) + geom_point(shape=1)
+ggplot(heights_df, aes(x=ed, y=earn)) + geom_point()
 
 ## Re-create the three scatterplots and add a regression trend line using
 ## the `geom_smooth()` function
 ## `height` vs. `earn`
-ggplot(heights_df, aes(x=height, y=earn)) + geom_point(shape=1) + geom_smooth()
+ggplot(heights_df, aes(x=height, y=earn)) + geom_point() + geom_smooth()
 ## `age` vs. `earn`
-ggplot(heights_df, aes(x=age, y=earn)) + geom_point(shape=1) + geom_smooth()
+ggplot(heights_df, aes(x=age, y=earn)) + geom_point() + geom_smooth()
 ## `ed` vs. `earn`
-ggplot(heights_df, aes(x=ed, y=earn)) + geom_point(shape=1) + geom_smooth()
+ggplot(heights_df, aes(x=ed, y=earn)) + geom_point() + geom_smooth()
 
 ## Create a scatterplot of `height`` vs. `earn`.  Use `sex` as the `col` (color) attribute
-ggplot(heights_df, aes(x=height, y=earn, col=sex)) + geom_point(shape=1)
+ggplot(heights_df, aes(x=height, y=earn, col=sex)) + geom_point()
 
 ## Using `ggtitle()`, `xlab()`, and `ylab()` to add a title, x label, and y label to the previous plot
 ## Title: Height vs. Earnings
 ## X label: Height (Inches)
 ## Y Label: Earnings (Dollars)
-ggplot(heights_df, aes(x=height, y=earn, col=sex)) + geom_point(shape=1) + 
+ggplot(heights_df, aes(x=height, y=earn, col=sex)) + geom_point() + 
   ggtitle("Height vs. Earnings") + xlab("Height (Inches)") + 
   ylab("Earnings (Dollars)")
 
