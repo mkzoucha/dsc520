b412aa76a30629fac566c7e9d4dcef97e0a90331
mkzoucha <mzoucha@my365.bellevue.edu>
2021-09-20 16:11:18 -0600

    Week 4 Assignment 1

---
+++ zoucham_completed/ZouchaM_Week4_Assignment_1.R
@@ -0,0 +1,11 @@
+# Assignment: Week 4 Assignment 1
+# Name: Zoucha, Michael
+# Date: 2021-09-19
+
+## Load the ggplot2 package
+library(ggplot2)
+theme_set(theme_minimal())
+
+## Load scores.csv to dataframe
+scores_df <- read.csv("data/scores.csv")
+
