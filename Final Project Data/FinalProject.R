d405a4c5898a7701776974f7ceafd0bdc9a3cafb
mkzoucha <mzoucha@my365.bellevue.edu>
2021-10-27 09:31:49 -0600

    Homework

--- Final Project Data/FinalProject.R
+++ Final Project Data/FinalProject.R
@@ -12,6 +12,7 @@ library(grid)
 library(gridExtra) 
 library(ggpubr)    
 library(patchwork)
+library(pivot)
 
 ## STEP 1: GATHER AND ANALYZE SALES DATA FOR FUTURE SALES NUMBER PREDICTIONS
 
@@ -98,25 +99,40 @@ final_merged_df <- dplyr::mutate(final_merged_df,
                    dplyr::mutate(., totalprofit = LineTotal - (OrderQty * StandardCost)) %>%
                    dplyr::mutate(., itemprofit = (LineTotal - (OrderQty * StandardCost)) / OrderQty)
 
+str(final_merged_df)
 
 ## Exploratory Data Analysis
+
 ## History of number of items sold for each ProductID for each year
 products_by_year<- dplyr::group_by(final_merged_df, year, ProductID) %>% 
+  dplyr::summarize(number_sold = sum(OrderQty))
+
+## History of number of items sold for each ProductID for each year
+products_by_year_month <- dplyr::group_by(final_merged_df, year, month, ProductID) %>% 
   dplyr::summarize(number_sold = sum(OrderQty))
 
 ## History of number of items sold for each ProductCategory for each year - include bar graph
 product_cat_by_year <- dplyr::group_by(final_merged_df, year, ProductCategory) %>% 
   dplyr::summarize(number_sold = sum(OrderQty))
 
+## History of number of items sold for each ProductCategory for each year
+product_cat_by_year_month <- dplyr::group_by(final_merged_df, year, month, ProductCategory) %>% 
+  dplyr::summarize(number_sold = sum(OrderQty))
+
+## History of number of items sold for each ProductSubCategory for each year - include bar graph
+product_subcat_by_year <- dplyr::group_by(final_merged_df, year, Subcategory, ProductCategory) %>% 
+  dplyr::summarize(number_sold = sum(OrderQty))
+
+## History of number of items sold for each ProductSubCategory for each year
+product_subcat_by_year_month <- dplyr::group_by(final_merged_df, year, month, Subcategory) %>% 
+  dplyr::summarize(number_sold = sum(OrderQty))
+
+
 cat_by_year_plot <- ggplot2::ggplot(product_cat_by_year, aes(x=year, y=number_sold, fill =ProductCategory)) + 
                 ggplot2::geom_bar(stat="identity", position=position_dodge()) + 
                 ggplot2::ggtitle("Number Items Sold per Year by Category") + 
                 ggplot2::xlab("Year") + 
                 ggplot2::ylab("Number Sold")
-
-## History of number of items sold for each ProductSubCategory for each year - include bar graph
-product_subcat_by_year <- dplyr::group_by(final_merged_df, year, Subcategory, ProductCategory) %>% 
-  dplyr::summarize(number_sold = sum(OrderQty))
 
 bikes_plot <- ggplot2::ggplot(filter(product_subcat_by_year, ProductCategory == "Bikes"), aes(x=year, y=number_sold, fill=Subcategory)) +
                 ggplot2::geom_bar(stat="identity", position=position_dodge()) +
@@ -159,13 +175,8 @@ print(accessories_plot, vp = viewport(layout.pos.row = 2, layout.pos.col =  1))
 print(clothing_plot, vp = viewport(layout.pos.row = 2, layout.pos.col =  2))
 
 
-## History of number of items sold for each ProductID for each year
-products_by_year_month <- dplyr::group_by(final_merged_df, year, month, ProductID) %>% dplyr::summarize(number_sold = sum(OrderQty))
-## History of number of items sold for each ProductCategory for each year
-product_cat_by_year_month <- dplyr::group_by(final_merged_df, year, month, ProductCategory) %>% dplyr::summarize(number_sold = sum(OrderQty))
-## History of number of items sold for each ProductSubCategory for each year
-product_subcat_by_year_month <- dplyr::group_by(final_merged_df, year, month, Subcategory) %>% dplyr::summarize(number_sold = sum(OrderQty))
+pivot_table_1 <- spread(filter(product_subcat_by_year, ProductCategory == "Bikes"), key = year, value = number_sold)
 
-
+filter(product_subcat_by_year, ProductCategory == "Bikes")
 ## STEP 2: CREATE PREDICTION MODEL FOR QUANTITY SOLD PER ITEM, PER MONTH, PER YEAR
 ## STEP 3: COMPARE TO CURRENT INVENTORY / INVENTORY MAX/MINS 
